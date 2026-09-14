a307d0bf304e7dd67c2dd3f6d1e0a255d0f57280
Juanes <jaramillojuan245@gmail.com>
2026-09-14 00:15:38 -0500

    Adding docker

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -10,9 +10,8 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on environment variables")
 	}
 
 	cfg := config.Load()
